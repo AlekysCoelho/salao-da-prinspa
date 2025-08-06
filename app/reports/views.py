ea519160289840d0b417980531cbc796dbcaa321
AlekysCoelho <alekyscoelho@gmail.com>
2025-08-05 23:36:12 -0300

    Corrigindo variáveis de ambiente .env

--- app/reports/views.py
+++ app/reports/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# reports/views.py
 
-# Create your views here.
+from celery.result import AsyncResult
+from django.shortcuts import render
+from rolepermissions.decorators import has_role_decorator
+
+from .forms import ReportForm
+from .tasks import generate_report
+
+
+@has_role_decorator('manager')
+def report_view(request):
+    form = ReportForm()
+    report_result = None
+    task_id = request.GET.get("task_id")
+
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            task = generate_report.delay(
+                form.cleaned_data["start_date"].strftime("%Y-%m-%d"),
+                form.cleaned_data["end_date"].strftime("%Y-%m-%d")
+            )
+            return render(request, "reports/report_pending.html", {"task_id": task.id})
+
+    if task_id:
+        result = AsyncResult(task_id)
+        if result.ready():
+            report_result = result.result
+
+    return render(request, "reports/report.html", {"form": form, "report_result": report_result})
